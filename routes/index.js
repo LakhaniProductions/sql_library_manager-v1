c17c7bcfa9c7222edec9b28ef0f29d4a44f54c22
Adnan Lakhani <adnan@lakhaniproductions.com>
2022-06-09 14:50:50 -0400

    Update Book Validation completed

--- routes/index.js
+++ routes/index.js
@@ -68,7 +68,7 @@ router.post('/books/:id', asyncHandler(async(req, res) => {
     if(error.name === "SequelizeValidationError") {
       book = await Book.build(req.body);
       book.id = req.params.id;
-      res.render('new-book', {book, errors: error.errors, title:'New Book'})
+      res.render('update-book', {book, errors: error.errors, title:'Edit Book'})
     } else {
       throw error;
     }
