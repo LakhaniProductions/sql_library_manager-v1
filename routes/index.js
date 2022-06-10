09e4411abf28109582a2c117cf41363cec137d21
LakhaniIE <adnan.lakhani@ideaentity.com>
2022-06-10 16:43:02 -0400

    Search functionality start

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Book = require('../models').Book;
+const perPage = 5;
 
 /* Handler function to wrap each route. */
 
@@ -8,8 +9,6 @@ function asyncHandler(cb){
   return async(req, res, next) => {
     try {
       await cb(req, res, next)
-      const allBooks= await Book.findAll();
-      console.log(allBooks);
     } catch(error){
       // res.status(500).send(error);
       next(error);
@@ -17,30 +16,31 @@ function asyncHandler(cb){
   }
 }
 
-
 /* GET home page. */
 router.get('/', asyncHandler(async(req, res) => {
   res.redirect('/books');
 }));
 
+
+
 /* GET All books */
 router.get('/books', asyncHandler(async(req, res) => {
-  const books = await Book.findAll({limit:5});
+  const books = await Book.findAll({limit:perPage});
   const allBooks = await Book.findAll();
-  const buttons=Math.ceil(Object.keys(allBooks).length/5);
+  const buttons=Math.ceil(Object.keys(allBooks).length/perPage);
   res.render('index', {books, buttons, title:'Books'});
 }));
+
+
 
 router.get('/books/page:number', asyncHandler(async(req, res) => {
   const number=req.params.number;
-  const offsetVal= (number*5)-5;
-  const books = await Book.findAll({offset:offsetVal,limit:5});
+  const offset= (number*perPage)-perPage;
+  const books = await Book.findAll({offset,limit:perPage});
   const allBooks = await Book.findAll();
-  const buttons=Math.ceil(Object.keys(allBooks).length/5);
-  
+  const buttons=Math.ceil(Object.keys(allBooks).length/perPage);
   res.render('index', {books, buttons, title:'Books'});
 }));
-
 
 /* GET new book form */
 router.get('/books/new', (req, res) => {
@@ -91,6 +91,20 @@ router.post('/books/:id', asyncHandler(async(req, res) => {
     }
   }
 }));
+
+router.post('/books', asyncHandler(async(req, res) => {
+
+  const books = await Book.findAll({ where: {
+    [Op.or]: [{title:req.body}, {author:req.body}, {genre:req.body}, {year:req.body}]
+  }});
+
+  // res.render('index', {books, buttons, title:'Books'});
+  res.render('index', {books});
+
+    
+  
+}));
+
 
 /* Delete individual book */
 router.get('/books/:id/delete', asyncHandler(async(req, res) => {
